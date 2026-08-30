1308f36839c5b9e35e0157af92beb0794bf1f61a
Vaibhavi Singh <vaibhavisinghhh87@gmail.com>
2026-08-30 12:08:10 +0530

    add progress model

--- backend/routes/Progress.js
+++ backend/routes/Progress.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Progress from "../models/Progress.js";
+import Progress from "../models/progress.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
